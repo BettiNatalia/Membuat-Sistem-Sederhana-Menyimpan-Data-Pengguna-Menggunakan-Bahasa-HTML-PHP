cc7d878579fbd6e26221311c2a13fbf1ad5cf2e7
BettiNatalia <102140115+BettiNatalia@users.noreply.github.com>
2023-01-19 16:17:21 +0700

    Add files via upload

---
+++ tambah_pengguna.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Biodata</title>
+</head>
+<body bgcolor="#ff99ee">
+	<center>
+	<h1>FORM TAMBAH DATA</h1>
+	</center>
+	<form action="proses_tambah_data.php" method="POST">
+		<center>
+	<table border="1">
+		<tr>
+			<td>
+				NAMA
+			</td>
+			<td>
+				<INPUT name="namaku" TYPE="TEXT" PLACEHOLDER="nama anda">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				PASSWORD
+			</td>
+			<td>
+				<INPUT name="katasandiku" TYPE="PASSWORD" PLACEHOLDER="password anda">
+			</td>
+		</tr>
+		<tr>
+			
+			<td>
+				<INPUT  TYPE="submit" value="Kirim Data">
+			</td>
+		</tr>
+	</table>
+	</form>
+	</center>
+</body>
+</html>
